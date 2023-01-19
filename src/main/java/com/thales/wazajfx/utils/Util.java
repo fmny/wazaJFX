b1081bc24f907487f38e2c6aebfd9c306cbdf9b4
francis <f_mauny@hotmail.fr>
2023-01-19 10:27:17 +0100

    "save-19-01-2023"

---
+++ src/main/java/com/thales/wazajfx/utils/Util.java
@@ -0,0 +1,19 @@
+package com.thales.wazajfx.utils;
+
+import javafx.concurrent.Task;
+
+public class Util {
+
+    public static void delay(long millis, Runnable continuation) {
+        Task<Void> sleeper = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                try { Thread.sleep(millis); }
+                catch (InterruptedException e) { }
+                return null;
+            }
+        };
+        sleeper.setOnSucceeded(event -> continuation.run());
+        new Thread(sleeper).start();
+    }
+}
